4fcd127d0118e0176b84a8f775fb5765165d28b0
reynaldo <saga4@hotmail.com>
2019-11-16 22:27:40 -0500

    visual basic 1.0

---
+++ CapaNegocio/CLCliente.vb
@@ -0,0 +1,9 @@
+﻿Imports CapaDato
+
+Public Class CLCliente
+
+    Public Shared Function listarCliente() As DataTable
+        Dim objCD As New DAOCliente
+        Return objCD.Ins.listarCliente()
+    End Function
+End Class
